ffee78228b9befd327546cbba4ec64edc2f00f77
VietTung0801 <tunle0801@gmail.com>
2022-09-19 20:56:58 +0700

    hooks update

---
+++ src/Hooks/Create/HandleCreateStore.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+//firebase
+import {db,storage} from '../../firebase/config'
+import {setDoc, doc} from 'firebase/firestore'
+import {ref, uploadBytes, getDownloadURL} from 'firebase/storage'
+
+
+
+function HandleCreateStore(owner, storeid, name, logoFile) {
+    return new Promise((resolve, reject)=>{
+        const logoRef = ref(storage, `stores/${storeid}/storeConfig/logo`)
+        uploadBytes(logoRef, logoFile)
+        .then(()=>{
+            console.log('logo done!')
+            getDownloadURL(logoRef)
+            .then((url)=>{
+                console.log('logo url done!')
+                const newStore = {
+                    ownerID: owner,
+                    storeName: name,
+                    logoLink: url,
+                    storeID: storeid,
+                }
+                setDoc(doc(db, 'store collection', storeid),  newStore)
+                .then(()=>{
+                    console.log('set store done!')
+                    resolve('congrats! Create store success')
+                })
+                .catch((error)=>{alert(error)})
+            })
+            .catch((error)=>{alert(error)})
+            })
+        .catch((error)=>{alert(error)})
+    })
+    
+}
+
+export {HandleCreateStore}
--- src/Screens/Console/Components/NewStorePage.js
+++ src/Screens/Console/Components/NewStorePage.js
@@ -1,16 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import {useNavigate} from 'react-router-dom'
 //context
 import {UserContext} from '../../../App'
+//hooks
+import {HandleCreateStore} from '../../../Hooks/Create/HandleCreateStore'
 
 function NewStorePage() {
-    //context
+    //
     const user = useContext(UserContext)
     const userid = user?.uid
+    const navigateTo = useNavigate()
+    
 
     //
+    const [storeName, setStoreName] = useState()
+    const [storeid, setstoreid] = useState()
+    const [logo, setLogo] = useState()
   return (
     <>
         <h2>New Store</h2>
+        <input type={'file'} onChange={(e)=>{setLogo(e.target.files[0])}}/>
+        <button onClick={()=>{
+            HandleCreateStore('booleke', 'bbbb', 'testname', logo)
+            .then(()=>{navigateTo('/console')})
+        }}>Test Do not click without carefull investigation</button>
     </>
   )
 }
