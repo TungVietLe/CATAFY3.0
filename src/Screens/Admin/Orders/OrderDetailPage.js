0dfb42bd6749b49091618070992a01a0d60f1005
VietTung0801 <tunle0801@gmail.com>
2022-09-21 21:27:38 +0700

    order detail cart fetch

--- src/Screens/Admin/Orders/OrderDetailPage.js
+++ src/Screens/Admin/Orders/OrderDetailPage.js
@@ -1,15 +1,40 @@
-import React from 'react'
+import { doc } from 'firebase/firestore'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
+import { db } from '../../../firebase/config'
+import { useReadOneDoc } from '../../../Hooks/FetchData/useReadOneDoc'
 
 function OrderDetailPage() {
     //params
-    const {orderidURL} = useParams()
+    const {storeidURL, orderidURL} = useParams()
+    //hooks
+    const {handleReadOneDoc, resultDoc} = useReadOneDoc()
+
+    
+    //
+    useEffect(()=>{
+        const storeDoc = doc(db, 'store collection', storeidURL)
+        const orderDoc = doc(storeDoc, 'orders', orderidURL)
+        handleReadOneDoc(orderDoc)
+    }, [])
+
+    const orderCart = resultDoc?.data()?.cart
 
   return (
-    <div>
+    <>
         <h1>Order Detail Page</h1>
         <p>params: {orderidURL}</p>
-    </div>
+
+        {/* ______ ORDER CART LIST ______ */}
+        {orderCart?.map((item)=>{
+
+            return <div key={item.productName}>
+                <img src={item.productImageLink} width='150'/>
+                <h3>{item.productName}</h3>
+            </div>
+        })}
+        {/* ______ ORDER CART LIST ______ */}
+    </>
   )
 }
 
