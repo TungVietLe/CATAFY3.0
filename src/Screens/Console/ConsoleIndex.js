02de6d53f02acf3e2745e3e5aff4613ed9a3d3a5
VietTung0801 <tunle0801@gmail.com>
2022-09-22 17:27:25 +0700

    scss

--- src/Screens/Console/ConsoleIndex.js
+++ src/Screens/Console/ConsoleIndex.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 //firebase
 import { collection } from 'firebase/firestore';
@@ -27,18 +27,24 @@ function ConsoleIndex() {
 
 
   return (
-    <>
+    <div className='ConsoleIndex'>
       <h1>Console index</h1>
 
       {filteredArray.map((userStore)=>{
-        return <div key={userStore.id}>
+        return <div className='storeContainer' key={userStore.id}>
           <img src={userStore.data().logoLink} width='100'/>
-          <h2>{userStore.data().storeName}</h2>
-          <button onClick={()=>{navigateTo(`/${userStore.data().storeID}`)}}>View store</button>
+          <div className='rightSide'>
+            <h2>{userStore.data().storeName}</h2>
+            <p>id: {userStore.id}</p>
+            <div className='buttonContainer'>
+              <Link className='Link Pri' onClick={()=>{navigateTo(`/${userStore.data().storeID}/admin`)}} target='blank'>Admin</Link>
+              <Link className='Link Sec' to={`/${userStore.data().storeID}`} target='blank'>View store</Link>
+            </div>
+          </div>
         </div>
       })}
 
-    </>
+    </div>
   )
 }
 
