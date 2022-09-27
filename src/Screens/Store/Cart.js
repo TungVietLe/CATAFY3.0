4aff16d146ae029403cecdc6ea25d62619487795
VietTung0801 <tunle0801@gmail.com>
2022-09-27 18:25:28 +0700

    store scss updated

--- src/Screens/Store/Cart.js
+++ src/Screens/Store/Cart.js
@@ -18,36 +18,38 @@ function Cart({openCart}) {
 
 
   return (
-    <div className='CartDrawer'>
-        <div className='CartHeader'>
-            <h2>Your Cart</h2>
-            <button onClick={()=>{openCart(false)}}>X</button>
-        </div>
-            <button onClick={()=>{refreshCart()}}>refresh</button>
-            <button onClick={()=>{localStorage.removeItem(storeidURL)}}>Clear</button>
-
-        {cart && <div className='CartFooter'>
-            <Link className='button Pri' to={'checkout'} onClick={()=>openCart(false)}>Checkout</Link> {/* only available when cart has item inside */}
-        </div>}
-        
-
-
-
-        {/* CART ITEMS LIST */}
-        {cart?.map((itemInCart)=>{
-            return <div key={itemInCart.name}>
-                <img src={itemInCart.thumbnailLink} width='150'/>
-                <h4>{itemInCart.name}</h4>
-                <p>quantity: {itemInCart.quantity}</p>
-                <button 
-                    onClick={()=>{
-                        handleAddItemToLocalCart(storeidURL, itemInCart.productName, itemInCart.productPrice, itemInCart.productImageLink)
-                        .then(()=>{refreshCart()})
-                }}>+</button>
+    <div className='cartBackground'>
+        <div className='CartDrawer'>
+            <div className='CartHeader'>
+                <h2>Your Cart</h2>
+                <button onClick={()=>{openCart(false)}}>X</button>
             </div>
-        })}
-        {/* CART ITEMS LIST */}
+                <button onClick={()=>{refreshCart()}}>refresh</button>
+                <button onClick={()=>{localStorage.removeItem(storeidURL)}}>Clear</button>
 
+            {cart && <div className='CartFooter'>
+                <Link className='button Pri' to={'checkout'} onClick={()=>openCart(false)}>Checkout</Link> {/* only available when cart has item inside */}
+            </div>}
+            
+
+
+
+            {/* CART ITEMS LIST */}
+            {cart?.map((itemInCart)=>{
+                return <div key={itemInCart.name}>
+                    <img src={itemInCart.thumbnailLink} width='150'/>
+                    <h4>{itemInCart.name}</h4>
+                    <p>quantity: {itemInCart.quantity}</p>
+                    <button 
+                        onClick={()=>{
+                            handleAddItemToLocalCart(storeidURL, itemInCart.productName, itemInCart.productPrice, itemInCart.productImageLink)
+                            .then(()=>{refreshCart()})
+                    }}>+</button>
+                </div>
+            })}
+            {/* CART ITEMS LIST */}
+
+        </div>
     </div>
   )
 }
