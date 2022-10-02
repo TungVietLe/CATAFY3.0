dd6555411e0eb52952ad81cb22d83db9b6505727
VietTung0801 <tunle0801@gmail.com>
2022-10-02 12:19:40 +0700

    add product loading screen

--- src/Screens/Admin/Products/ProductNew.js
+++ src/Screens/Admin/Products/ProductNew.js
@@ -1,45 +1,72 @@
 import React, { useContext, useState } from 'react'
-import {useNavigate, useParams} from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 //context
-import {UserContext} from '../../../App'
+import { UserContext } from '../../../App'
 //hooks
-import {handleCreateProduct} from '../../../Hooks/Create/handleCreateProduct'
+import { handleCreateProduct } from '../../../Hooks/Create/handleCreateProduct'
+//screens
+import LoadingScreen from '../../Loading/LoadingScreen'
 
 function ProductNew() {
-  const navigateTo = useNavigate()
-  //context
-  const user = useContext(UserContext); const userid = user?.uid
-  //params
-  const {storeidURL} = useParams()
-  //hooks
-  
+	const navigateTo = useNavigate()
+	//context
+	const user = useContext(UserContext)
+	const userid = user?.uid
+	//params
+	const { storeidURL } = useParams()
+	//hooks
 
-  
+	//
+	const [isAddingItem, setLoadingAddItem] = useState(false)
 
-  //
-  const [productName, setProductName] = useState()
-  const [productPrice, setProductPrice] = useState()
-  const [productImg, setProductImg] = useState()
+	const [productName, setProductName] = useState()
+	const [productPrice, setProductPrice] = useState()
+	const [productImg, setProductImg] = useState()
 
+	return (
+		<>
+			{isAddingItem && <LoadingScreen label={'Adding Product To Store'} />}
+			<h2>New Store</h2>
+			{/* _____ INPUTS _____ */}
+			<input
+				placeholder="item name"
+				onChange={(e) => {
+					setProductName(e.target.value)
+				}}
+			/>
+			<input
+				placeholder="price"
+				type={'number'}
+				onChange={(e) => {
+					setProductPrice(e.target.value)
+				}}
+			/>
+			<input
+				type={'file'}
+				onChange={(e) => {
+					setProductImg(e.target.files[0])
+				}}
+			/>
+			{/* _____ INPUTS _____ */}
 
-
-
-
-  return (
-    <>
-        <h2>New Store</h2>
-        {/* _____ INPUTS _____ */}
-        <input placeholder='item name' onChange={(e)=>{setProductName(e.target.value)}}/>
-        <input placeholder='price' type={'number'} onChange={(e)=>{setProductPrice(e.target.value)}}/>
-        <input type={'file'} onChange={(e)=>{setProductImg(e.target.files[0])}}/>
-        {/* _____ INPUTS _____ */}
-
-        <button onClick={()=>{
-          handleCreateProduct(storeidURL, productName, productPrice, productImg)
-          .then((mes)=>{console.log(mes)})
-        }}>Save</button>
-    </>
-  )
+			<button
+				onClick={() => {
+					setLoadingAddItem(true)
+					handleCreateProduct(
+						storeidURL,
+						productName,
+						productPrice,
+						productImg
+					).then((mes) => {
+						setLoadingAddItem(false)
+						console.log(mes)
+					})
+				}}
+			>
+				Save
+			</button>
+		</>
+	)
 }
 
 export default ProductNew
