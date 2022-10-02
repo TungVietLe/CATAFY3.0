455203846746539d0a98781a2c49fc73b16e2294
VietTung0801 <tunle0801@gmail.com>
2022-10-02 17:33:40 +0700

    admin logo and styling nav

--- src/Routes/AdminRoutes.js
+++ src/Routes/AdminRoutes.js
@@ -1,60 +1,71 @@
 import React, { useContext, useEffect, useState } from 'react'
-import {BrowserRouter as Router, Routes, Route, Link, useParams} from "react-router-dom";
-import {UserContext} from '../App'
+import { Routes, Route, Link, useParams, useNavigate, NavLink } from 'react-router-dom'
+import { UserContext } from '../App'
+import logo from '../logo.svg'
 
-import ProductRoutes from './AdminRoutes/ProductRoutes';
-import OrderRoutes from './AdminRoutes/OrderRoutes';
+import ProductRoutes from './AdminRoutes/ProductRoutes'
+import OrderRoutes from './AdminRoutes/OrderRoutes'
 
 //firebase
-import { doc } from 'firebase/firestore';
-import {db} from '../firebase/config'
+import { doc } from 'firebase/firestore'
+import { db } from '../firebase/config'
 //hooks
-import {handleReadOneDoc} from '../Hooks/FetchData/useReadOneDoc'
-
+import { handleReadOneDoc } from '../Hooks/FetchData/useReadOneDoc'
 
 function AdminRoutes() {
-    //params
-    const {storeidURL} = useParams()
-    //context
-    const user = useContext(UserContext)
-    const userid = user?.uid
+	const navigateTo = useNavigate()
+	//params
+	const { storeidURL } = useParams()
+	//context
+	const user = useContext(UserContext)
+	const userid = user?.uid
 
-    //
-    useEffect(()=>{
-        handleReadOneDoc(doc(db, 'store collection', storeidURL))
-        .then((res)=>{setstoreConfig(res)})
-    }, [])
+	//
+	useEffect(() => {
+		handleReadOneDoc(doc(db, 'store collection', storeidURL)).then((res) => {
+			setstoreConfig(res)
+		})
+	}, [])
 
-    const [storeConfig, setstoreConfig] = useState(); const storeConfigData = storeConfig?.data()
-    const storeOwnerid = storeConfigData?.ownerID
-    const userIsOwner = user && storeConfig && storeOwnerid == userid // user exist and store exist and storeownerid = userid
+	const [storeConfig, setstoreConfig] = useState()
+	const storeConfigData = storeConfig?.data()
+	const storeOwnerid = storeConfigData?.ownerID
+	const userIsOwner = user && storeConfig && storeOwnerid == userid // user exist and store exist and storeownerid = userid
 
+	return (
+		<>
+			{userIsOwner ? (
+				<div className="AdminRoutes">
+					<div className="AdminNav">
+						<img
+							src={logo}
+							onClick={() => {
+								navigateTo('/console')
+							}}
+						/>
+						<Link to={''}>Overview</Link>
+						<NavLink to={'orders'}>Orders</NavLink>
+						<NavLink to={'products'}>Product</NavLink>
+						<NavLink to={'setting'}>Setting</NavLink>
+						<Link to={`/${storeidURL}`} target="_blank">
+							View Store
+						</Link>
+					</div>
 
-
-
-
-
-  return (
-    <>
-        {userIsOwner? <div className='AdminRoutes'>
-            <div className='AdminNav'>
-                <Link to={''}>Overview</Link>
-                <Link to={'orders'}>Orders</Link>
-                <Link to={'products'}>Product</Link>
-                <Link to={'setting'}>Setting</Link>
-                <Link to={`/${storeidURL}`} target='_blank'>View Store</Link>
-            </div>
-            <Routes>
-                <Route index element={<div>Admin index</div>}/>
-                <Route path="orders/*" element={<OrderRoutes/>}/>
-                <Route path="products/*" element={<ProductRoutes/>}/>
-                <Route path="setting" element={<div>setting</div>}/>
-            </Routes>
-        </div> : <>
-            <div>Sorry you are not owner</div>
-        </>}
-    </>
-  )
+					<Routes>
+						<Route index element={<div>Admin index</div>} />
+						<Route path="orders/*" element={<OrderRoutes />} />
+						<Route path="products/*" element={<ProductRoutes />} />
+						<Route path="setting" element={<div>setting</div>} />
+					</Routes>
+				</div>
+			) : (
+				<>
+					<div>Sorry you are not owner</div>
+				</>
+			)}
+		</>
+	)
 }
 
 export default AdminRoutes
